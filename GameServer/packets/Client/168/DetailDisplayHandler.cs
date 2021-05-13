5d700a9bfe603287bebfe28e4a9ab55c005904cc
Jérémy Virant <jeremy@virant.fr>
2021-05-13 19:34:35 +0200

    change item info: always show usable classes and description
    (cherry picked from commit fe2f4145e395c8a43535482c80eab64264b2bb83)

--- GameServer/packets/Client/168/DetailDisplayHandler.cs
+++ GameServer/packets/Client/168/DetailDisplayHandler.cs
@@ -163,25 +163,24 @@ namespace DOL.GS.PacketHandler.Client.v168
 							objectInfo.Add(" ");
 						}
 
+						WriteUsableClasses(objectInfo, invItem, client);
+
 						if (invItem.Object_Type >= (int)eObjectType.GenericWeapon
 							&& invItem.Object_Type <= (int)eObjectType._LastWeapon
 							|| invItem.Object_Type == (int)eObjectType.Instrument)
 						{
-							WriteUsableClasses(objectInfo, invItem, client);
 							WriteMagicalBonuses(objectInfo, invItem, client, false);
 							WriteClassicWeaponInfos(objectInfo, invItem, client);
 						}
 
 						if (invItem.Object_Type >= (int)eObjectType.Cloth && invItem.Object_Type <= (int)eObjectType.Scale)
 						{
-							WriteUsableClasses(objectInfo, invItem, client);
 							WriteMagicalBonuses(objectInfo, invItem, client, false);
 							WriteClassicArmorInfos(objectInfo, invItem, client);
 						}
 
 						if (invItem.Object_Type == (int)eObjectType.Shield)
 						{
-							WriteUsableClasses(objectInfo, invItem, client);
 							WriteMagicalBonuses(objectInfo, invItem, client, false);
 							WriteClassicShieldInfos(objectInfo, invItem, client);
 						}
@@ -1046,7 +1045,10 @@ namespace DOL.GS.PacketHandler.Client.v168
 					output.Add(" ");
 
 					ISpellHandler sh = ScriptMgr.CreateSpellHandler(client.Player, s, SkillBase.GetSpellLine(GlobalSpellsLines.Reserved_Spells));
-					Util.AddRange(output, sh.DelveInfo);
+					if (sh != null)
+						Util.AddRange(output, sh.DelveInfo);
+					else
+						output.Add($"Subspell {s.Name} ({s.ID}) is not implemented");
 				}
 			}
 			if (client.Account.PrivLevel > 1)
