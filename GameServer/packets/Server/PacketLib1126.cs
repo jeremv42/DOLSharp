f301cd3b6b4e7b52298cc4df4131fc57abab5a9c
Jérémy Virant <jeremy@virant.fr>
2023-04-10 19:15:11 +0200

    fix friend window in 1.126+

--- GameServer/packets/Server/PacketLib1126.cs
+++ GameServer/packets/Server/PacketLib1126.cs
@@ -31,8 +31,8 @@ namespace DOL.GS.PacketHandler
 	{
 		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-        protected override byte ServerTypeID
-        {
+		protected override byte ServerTypeID
+		{
 			get
 			{
 				var serverType = GameServer.Instance.Configuration.ServerType;
@@ -45,7 +45,7 @@ namespace DOL.GS.PacketHandler
 			}
 		}
 
-        public PacketLib1126(GameClient client)
+		public PacketLib1126(GameClient client)
 			: base(client)
 		{
 		}
@@ -83,7 +83,7 @@ namespace DOL.GS.PacketHandler
 				// _gameClient.PacketProcessor.Encoding.EncryptionState = eEncryptionState.PseudoRC4Encrypted; // disabled by the launcher
 
 				// Reply with current version
-				pak.WriteString((((int)m_gameClient.Version) / 1000) + "." + (((int)m_gameClient.Version) - 1000), 5);
+				pak.WriteString((((int) m_gameClient.Version) / 1000) + "." + (((int) m_gameClient.Version) - 1000), 5);
 
 				// revision, last seen (c) 0x63
 				pak.WriteByte(0x00);
@@ -100,12 +100,12 @@ namespace DOL.GS.PacketHandler
 			if (realm < eRealm._FirstPlayerRealm || realm > eRealm._LastPlayerRealm)
 				throw new Exception($"CharacterOverview requested for unknown realm {realm}");
 
-			int firstSlot = (byte)realm * 100;
+			int firstSlot = (byte) realm * 100;
 
 			var enableRealmSwitcherBit = GameServer.ServerRules.IsAllowedCharsInAllRealms(m_gameClient) ? 1 : 0;
 			using (GSTCPPacketOut pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.CharacterOverview1126)))
 			{
-				pak.WriteIntLowEndian((uint)enableRealmSwitcherBit); // 0x01 & 0x02 are flags
+				pak.WriteIntLowEndian((uint) enableRealmSwitcherBit); // 0x01 & 0x02 are flags
 				pak.WriteIntLowEndian(0);
 				pak.WriteIntLowEndian(0);
 				pak.WriteIntLowEndian(0);
@@ -127,12 +127,13 @@ namespace DOL.GS.PacketHandler
 						log.Error($"SendCharacterOverview - Duplicate char in slot? Slot: {c.AccountSlot}, Account: {c.AccountName}", ex);
 					}
 				}
+
 				var itemsByOwnerID = new Dictionary<string, Dictionary<eInventorySlot, InventoryItem>>();
 
 				if (charsBySlot.Any())
 				{
-					var filterBySlotPosition = DB.Column(nameof(InventoryItem.SlotPosition)).IsGreaterOrEqualTo((int)eInventorySlot.MinEquipable)
-						.And(DB.Column(nameof(InventoryItem.SlotPosition)).IsLessOrEqualTo((int)eInventorySlot.MaxEquipable));
+					var filterBySlotPosition = DB.Column(nameof(InventoryItem.SlotPosition)).IsGreaterOrEqualTo((int) eInventorySlot.MinEquipable)
+						.And(DB.Column(nameof(InventoryItem.SlotPosition)).IsLessOrEqualTo((int) eInventorySlot.MaxEquipable));
 					var allItems = DOLDB<InventoryItem>.SelectObjects(DB.Column(nameof(InventoryItem.OwnerID)).IsIn(charsBySlot.Values.Select(c => c.ObjectId)).And(filterBySlotPosition));
 
 					foreach (InventoryItem item in allItems)
@@ -142,7 +143,7 @@ namespace DOL.GS.PacketHandler
 							if (!itemsByOwnerID.ContainsKey(item.OwnerID))
 								itemsByOwnerID.Add(item.OwnerID, new Dictionary<eInventorySlot, InventoryItem>());
 
-							itemsByOwnerID[item.OwnerID].Add((eInventorySlot)item.SlotPosition, item);
+							itemsByOwnerID[item.OwnerID].Add((eInventorySlot) item.SlotPosition, item);
 						}
 						catch (Exception ex)
 						{
@@ -179,12 +180,12 @@ namespace DOL.GS.PacketHandler
 						extensionBoots = item.Extension;
 
 					string locationDescription = string.Empty;
-					Region region = WorldMgr.GetRegion((ushort)c.Region);
+					Region region = WorldMgr.GetRegion((ushort) c.Region);
 					if (region != null)
 						locationDescription = m_gameClient.GetTranslatedSpotDescription(region, c.Xpos, c.Ypos, c.Zpos);
 					string classname = "";
 					if (c.Class != 0)
-						classname = ((eCharacterClass)c.Class).ToString();
+						classname = ((eCharacterClass) c.Class).ToString();
 					string racename = m_gameClient.RaceToTranslatedName(c.Race, c.Gender);
 
 					charItems.TryGetValue(eInventorySlot.RightHandWeapon, out InventoryItem rightHandWeapon);
@@ -201,33 +202,33 @@ namespace DOL.GS.PacketHandler
 
 					ushort rightHandColor = 0;
 					if (rightHandWeapon != null)
-						rightHandColor = (ushort)(rightHandWeapon.Emblem != 0 ? rightHandWeapon.Emblem : rightHandWeapon.Color);
+						rightHandColor = (ushort) (rightHandWeapon.Emblem != 0 ? rightHandWeapon.Emblem : rightHandWeapon.Color);
 					ushort helmetColor = 0;
 					if (helmet != null)
-						helmetColor = (ushort)(helmet.Emblem != 0 ? helmet.Emblem : helmet.Color);
+						helmetColor = (ushort) (helmet.Emblem != 0 ? helmet.Emblem : helmet.Color);
 					ushort glovesColor = 0;
 					if (gloves != null)
-						glovesColor = (ushort)(gloves.Emblem != 0 ? gloves.Emblem : gloves.Color);
+						glovesColor = (ushort) (gloves.Emblem != 0 ? gloves.Emblem : gloves.Color);
 					ushort bootsColor = 0;
 					if (boots != null)
-						bootsColor = (ushort)(boots.Emblem != 0 ? boots.Emblem : boots.Color);
+						bootsColor = (ushort) (boots.Emblem != 0 ? boots.Emblem : boots.Color);
 					ushort leftHandWeaponColor = 0;
 					if (leftHandWeapon != null)
-						leftHandWeaponColor = (ushort)(leftHandWeapon.Emblem != 0 ? leftHandWeapon.Emblem : leftHandWeapon.Color);
+						leftHandWeaponColor = (ushort) (leftHandWeapon.Emblem != 0 ? leftHandWeapon.Emblem : leftHandWeapon.Color);
 					ushort torsoColor = 0;
 					if (torso != null)
-						torsoColor = (ushort)(torso.Emblem != 0 ? torso.Emblem : torso.Color);
+						torsoColor = (ushort) (torso.Emblem != 0 ? torso.Emblem : torso.Color);
 					ushort cloakColor = 0;
 					if (cloak != null)
-						cloakColor = (ushort)(cloak.Emblem != 0 ? cloak.Emblem : cloak.Color);
+						cloakColor = (ushort) (cloak.Emblem != 0 ? cloak.Emblem : cloak.Color);
 					ushort legsColor = 0;
 					if (legs != null)
-						legsColor = (ushort)(legs.Emblem != 0 ? legs.Emblem : legs.Color);
+						legsColor = (ushort) (legs.Emblem != 0 ? legs.Emblem : legs.Color);
 					ushort armsColor = 0;
 					if (arms != null)
-						armsColor = (ushort)(arms.Emblem != 0 ? arms.Emblem : arms.Color);
+						armsColor = (ushort) (arms.Emblem != 0 ? arms.Emblem : arms.Color);
 
-					pak.WriteByte((byte)c.Level);
+					pak.WriteByte((byte) c.Level);
 					pak.WritePascalStringIntLE(c.Name, 0x18);
 					pak.WriteIntLowEndian(0x18);
 					pak.WriteByte(1); // always 1 ?
@@ -237,30 +238,30 @@ namespace DOL.GS.PacketHandler
 					pak.WriteByte(c.HairColor);
 					pak.WriteByte(c.FaceType); // seems to be : 0xF0 = face
 					pak.WriteByte(c.HairStyle); // seems to be : 0xF0 = hair
-					pak.WriteByte((byte)((extensionBoots << 4) | extensionGloves));
-					pak.WriteByte((byte)((extensionTorso << 4) | (c.IsCloakHoodUp ? 0x1 : 0x0)));
+					pak.WriteByte((byte) ((extensionBoots << 4) | extensionGloves));
+					pak.WriteByte((byte) ((extensionTorso << 4) | (c.IsCloakHoodUp ? 0x1 : 0x0)));
 					pak.WriteByte(c.CustomisationStep); //1 = auto generate config, 2= config ended by player, 3= enable config to player
 					pak.WriteByte(c.MoodType);
 					pak.Fill(0x0, 13);
 					pak.WritePascalStringIntLE(locationDescription, 0x18);
 					pak.WritePascalStringIntLE(classname, 0x18);
 					pak.WritePascalStringIntLE(racename, 0x18);
-					pak.WriteShortLowEndian((ushort)c.CurrentModel);
+					pak.WriteShortLowEndian((ushort) c.CurrentModel);
 
-					pak.WriteByte((byte)c.Region);
-					if (region == null || (int)m_gameClient.ClientType > region.Expansion)
+					pak.WriteByte((byte) c.Region);
+					if (region == null || (int) m_gameClient.ClientType > region.Expansion)
 						pak.WriteByte(0x00);
 					else
-						pak.WriteByte((byte)(region.Expansion + 1)); //0x04-Cata zone, 0x05 - DR zone
+						pak.WriteByte((byte) (region.Expansion + 1)); //0x04-Cata zone, 0x05 - DR zone
 
-					pak.WriteShortLowEndian((ushort)(helmet != null ? helmet.Model : 0));
-					pak.WriteShortLowEndian((ushort)(gloves != null ? gloves.Model : 0));
-					pak.WriteShortLowEndian((ushort)(boots != null ? boots.Model : 0));
+					pak.WriteShortLowEndian((ushort) (helmet != null ? helmet.Model : 0));
+					pak.WriteShortLowEndian((ushort) (gloves != null ? gloves.Model : 0));
+					pak.WriteShortLowEndian((ushort) (boots != null ? boots.Model : 0));
 					pak.WriteShortLowEndian(rightHandColor);
-					pak.WriteShortLowEndian((ushort)(torso != null ? torso.Model : 0));
-					pak.WriteShortLowEndian((ushort)(cloak != null ? cloak.Model : 0));
-					pak.WriteShortLowEndian((ushort)(legs != null ? legs.Model : 0));
-					pak.WriteShortLowEndian((ushort)(arms != null ? arms.Model : 0));
+					pak.WriteShortLowEndian((ushort) (torso != null ? torso.Model : 0));
+					pak.WriteShortLowEndian((ushort) (cloak != null ? cloak.Model : 0));
+					pak.WriteShortLowEndian((ushort) (legs != null ? legs.Model : 0));
+					pak.WriteShortLowEndian((ushort) (arms != null ? arms.Model : 0));
 
 					pak.WriteShortLowEndian(helmetColor);
 					pak.WriteShortLowEndian(glovesColor);
@@ -272,40 +273,41 @@ namespace DOL.GS.PacketHandler
 					pak.WriteShortLowEndian(armsColor);
 
 					//weapon models
-					pak.WriteShortLowEndian((ushort)(rightHandWeapon != null ? rightHandWeapon.Model : 0));
-					pak.WriteShortLowEndian((ushort)(leftHandWeapon != null ? leftHandWeapon.Model : 0));
-					pak.WriteShortLowEndian((ushort)(twoHandWeapon != null ? twoHandWeapon.Model : 0));
-					pak.WriteShortLowEndian((ushort)(distanceWeapon != null ? distanceWeapon.Model : 0));
+					pak.WriteShortLowEndian((ushort) (rightHandWeapon != null ? rightHandWeapon.Model : 0));
+					pak.WriteShortLowEndian((ushort) (leftHandWeapon != null ? leftHandWeapon.Model : 0));
+					pak.WriteShortLowEndian((ushort) (twoHandWeapon != null ? twoHandWeapon.Model : 0));
+					pak.WriteShortLowEndian((ushort) (distanceWeapon != null ? distanceWeapon.Model : 0));
 
-					pak.WriteByte((byte)c.Strength);
-					pak.WriteByte((byte)c.Quickness);
-					pak.WriteByte((byte)c.Constitution);
-					pak.WriteByte((byte)c.Dexterity);
-					pak.WriteByte((byte)c.Intelligence);
-					pak.WriteByte((byte)c.Piety);
-					pak.WriteByte((byte)c.Empathy); // ?
-					pak.WriteByte((byte)c.Charisma); // ?
+					pak.WriteByte((byte) c.Strength);
+					pak.WriteByte((byte) c.Quickness);
+					pak.WriteByte((byte) c.Constitution);
+					pak.WriteByte((byte) c.Dexterity);
+					pak.WriteByte((byte) c.Intelligence);
+					pak.WriteByte((byte) c.Piety);
+					pak.WriteByte((byte) c.Empathy); // ?
+					pak.WriteByte((byte) c.Charisma); // ?
 
-					pak.WriteByte((byte)c.Class);
-					pak.WriteByte((byte)c.Realm); // ok?
-					pak.WriteByte((byte)((((c.Race & 0x10) << 2) + (c.Race & 0x0F)) | (c.Gender << 4)));
-					if (c.ActiveWeaponSlot == (byte)GameLiving.eActiveWeaponSlot.TwoHanded)
+					pak.WriteByte((byte) c.Class);
+					pak.WriteByte((byte) c.Realm); // ok?
+					pak.WriteByte((byte) ((((c.Race & 0x10) << 2) + (c.Race & 0x0F)) | (c.Gender << 4)));
+					if (c.ActiveWeaponSlot == (byte) GameLiving.eActiveWeaponSlot.TwoHanded)
 					{
 						pak.WriteByte(0x02);
 						pak.WriteByte(0x02);
 					}
-					else if (c.ActiveWeaponSlot == (byte)GameLiving.eActiveWeaponSlot.Distance)
+					else if (c.ActiveWeaponSlot == (byte) GameLiving.eActiveWeaponSlot.Distance)
 					{
 						pak.WriteByte(0x03);
 						pak.WriteByte(0x03);
 					}
 					else
 					{
-						pak.WriteByte((byte)(rightHandWeapon != null ? 0x00 : 0xFF));
-						pak.WriteByte((byte)(leftHandWeapon != null ? 0x01 : 0xFF));
+						pak.WriteByte((byte) (rightHandWeapon != null ? 0x00 : 0xFF));
+						pak.WriteByte((byte) (leftHandWeapon != null ? 0x01 : 0xFF));
 					}
+
 					pak.WriteByte(0); // SI = 1, Classic = 0
-					pak.WriteByte((byte)c.Constitution); // ok
+					pak.WriteByte((byte) c.Constitution); // ok
 					pak.WriteByte(0); // unknown
 				}
 
@@ -346,26 +348,43 @@ namespace DOL.GS.PacketHandler
 				pak.WriteByte(0x00); //unk
 
 				// weapondamage
-				var wd = (int)(m_gameClient.Player.WeaponDamage(m_gameClient.Player.AttackWeapon) * 100);
-				pak.WriteByte((byte)(wd / 0x100));
+				var wd = (int) (m_gameClient.Player.WeaponDamage(m_gameClient.Player.AttackWeapon) * 100);
+				pak.WriteByte((byte) (wd / 0x100));
 				pak.WriteByte(0x00);
-				pak.WriteByte((byte)(wd % 0x100));
+				pak.WriteByte((byte) (wd % 0x100));
 				pak.WriteByte(0x00);
 				// weaponskill
 				int ws = m_gameClient.Player.DisplayedWeaponSkill;
-				pak.WriteByte((byte)(ws >> 8));
+				pak.WriteByte((byte) (ws >> 8));
 				pak.WriteByte(0x00);
-				pak.WriteByte((byte)(ws & 0xff));
+				pak.WriteByte((byte) (ws & 0xff));
 				pak.WriteByte(0x00);
 				// overall EAF
 				int eaf = m_gameClient.Player.EffectiveOverallAF;
-				pak.WriteByte((byte)(eaf >> 8));
+				pak.WriteByte((byte) (eaf >> 8));
 				pak.WriteByte(0x00);
-				pak.WriteByte((byte)(eaf & 0xff));
+				pak.WriteByte((byte) (eaf & 0xff));
 				pak.WriteByte(0x00);
 				SendTCP(pak);
 			}
 		}
+
+		public override void SendAddFriends(string[] friendNames)
+		{
+			using var pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.AddFriend));
+			pak.WriteByte((byte) friendNames.Length);
+			foreach (var friend in friendNames)
+				pak.WritePascalStringIntLE(friend);
+			SendTCP(pak);
+		}
+
+		public override void SendRemoveFriends(string[] friendNames)
+		{
+			using var pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.RemoveFriend));
+			pak.WriteByte(0x00);
+			foreach (var friend in friendNames)
+				pak.WritePascalStringIntLE(friend);
+			SendTCP(pak);
+		}
 	}
 }
-
